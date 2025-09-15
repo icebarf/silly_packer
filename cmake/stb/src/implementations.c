f4ba6903e467264f920bdf1cd7d7293df339196b
szejmon <szejmon-mail@proton.me>
2025-09-15 20:14:09 +0200

    Organized build system

---
+++ cmake/stb/src/implementations.c
@@ -0,0 +1,15 @@
+#ifdef STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+#endif
+
+#ifdef STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
+#endif
+
+#ifdef STB_IMAGE_RESIZE_IMPLEMENTATION
+#include <stb_image_resize2.h>
+#endif
+
+#ifdef STB_RECT_PACK_IMPLEMENTATION
+#include <stb_rect_pack.h>
+#endif
