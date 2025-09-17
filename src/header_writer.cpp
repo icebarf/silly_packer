492a78b7dd8a11ddf048c9ffb33c2bd5f239df68
szejmon <szejmon-mail@proton.me>
2025-09-17 22:01:09 +0200

    Added header_writer

---
+++ src/header_writer.cpp
@@ -0,0 +1,72 @@
+#include "header_writer.h"
+#include <format>
+#include <sstream>
+#include <stdexcept>
+
+header_writer::header_writer(const std::filesystem::path& path,
+                             const std::string& guard, bool use_stdint,
+                             const std::string& spacename)
+    : _fstream(path), _using_stdint(use_stdint) {
+  if (!_fstream.is_open()) {
+    throw std::runtime_error("Could not open file");
+  }
+
+  write(std::format("#ifndef {0}\n#define {0}\n", guard));
+  if (_using_stdint) {
+    write("#include <cstdint>\n");
+    _byte_type = "std::uint8_t";
+  } else {
+    write("static_assert(sizeof(unsigned char) == 1, \"unsigned char must be 1 "
+          "byte in size\");");
+    _byte_type = "unsigned char";
+  }
+
+  if (spacename != "") {
+    write(std::format("namespace {} {{", spacename));
+    _has_namespace = true;
+  }
+}
+
+bool header_writer::is_open() const { return _fstream.is_open(); }
+
+bool header_writer::using_stdint() const { return _using_stdint; }
+
+const std::string& header_writer::byte_type() const { return _byte_type; }
+
+void header_writer::write(const std::string& data) {
+  _fstream.write(data.c_str(), data.size());
+}
+
+void header_writer::write_variable(const std::string& type,
+                                   const std::string& name,
+                                   const std::string& value, bool constant) {
+  std::string constant_string = "";
+  if (constant) {
+    constant_string = "constexpr ";
+  }
+  write(std::format("{}{} {}={};", constant_string, type, name, value));
+}
+
+void header_writer::write_byte_array(const std::string& name,
+                                     const std::uint8_t* data, std::size_t size,
+                                     bool constant) {
+  std::stringstream bytes_stream;
+  for (std::size_t i = 0; i < size; i++) {
+    bytes_stream << static_cast<unsigned int>(data[i]) << ',';
+  }
+
+  std::string constant_string = "";
+  if (constant) {
+    constant_string = "constexpr ";
+  }
+  write(std::format("{}{} {}[{}]={{{}}};", constant_string, _byte_type, name,
+                    size, bytes_stream.str()));
+}
+
+void header_writer::close() {
+  if (_has_namespace) {
+    write("}");
+  }
+  write("\n#endif");
+  _fstream.close();
+}
---
+++ src/header_writer.h
@@ -0,0 +1,32 @@
+#ifndef SILLY_PACKER_HEADER_WRITER_H
+#define SILLY_PACKER_HEADER_WRITER_H
+
+#include <cstdint>
+#include <filesystem>
+#include <fstream>
+
+class header_writer {
+public:
+  header_writer(const std::filesystem::path& path, const std::string& guard,
+                bool use_stdint = true, const std::string& spacename = "");
+
+  bool is_open() const;
+  bool using_stdint() const;
+  const std::string& byte_type() const;
+
+  void write(const std::string& data);
+  void write_variable(const std::string& type, const std::string& name,
+                      const std::string& value, bool constant = true);
+  void write_byte_array(const std::string& name, const std::uint8_t* data,
+                        std::size_t size, bool constant = true);
+
+  void close();
+
+private:
+  std::ofstream _fstream;
+  bool _using_stdint;
+  bool _has_namespace;
+  std::string _byte_type;
+};
+
+#endif
