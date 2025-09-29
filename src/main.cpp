39dcc5abe80e2529294af0305193f9b8cfd86082
icebarf <sysgrammer@protonmail.com>
2025-09-29 18:37:26 +0530

    default algorithm changed

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,7 @@ struct packer_args : public argparse::Args {
   std::string& algorithm =
       kwarg("a,algorithm",
             "Use one of these algorithms to pack: maxrects, guillotine")
-          .set_default("maxrects");
+          .set_default("guillotine");
   bool& gpu_optimize =
       kwarg("g,gpu_optimize",
             "Extend input images to be squares with 2^n "
